30d213509bb7e10639a1a3b4f0adeb6274679e05
marianaG11 <mariana.garcia.16@icloud.com>
2022-06-11 23:49:25 -0500

    javascripting exercises

--- javascripting.js
+++ javascripting.js
@@ -20,6 +20,29 @@ let total = 0;
 const limit = 10;
 for (let i = 0;  i < limit; i++){
     total += i
-}
+};
 
 console.log(total);
+
+//function-arguments
+function math (firstArg, secondArg, thirdArg) {
+    return (secondArg * thirdArg) + firstArg
+};
+
+console.log(math(53, 61, 67));
+
+//if statement
+const fruit = 'mango';
+if (fruit.length > 5){
+    console.log('fruit has more than 5 characters');
+}else {
+    console.log('fruit name has 5 characters or less');
+};
+
+//looping through arrays
+const pets = ['dog', 'cat', 'hamster']
+for (let i = 0; i < 3; i++){
+    pets[i] = pets[i] + 's'
+};
+
+console.log(pets)
